65e5a88d6ac4e28aff0b2fb48a40b7f19227ae90
German Blanco <german.blanco@gmail.com>
2025-02-14 07:02:55 +0100

    nochmod

--- docker/build-docker.sh
+++ docker/build-docker.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-podman build -t quay.io/abranzyck/sshd-bastion:ubuntu1 .
-podman push quay.io/abranzyck/sshd-bastion:ubuntu1
+podman build -t quay.io/abranzyck/sshd-bastion:ubuntu3 .
+podman push quay.io/abranzyck/sshd-bastion:ubuntu3
