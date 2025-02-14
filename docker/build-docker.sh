d7a858b4df3efd1e959547045f8bd82e908279da
German Blanco <german.blanco@gmail.com>
2025-02-14 07:08:11 +0100

    createkeysfolder

--- docker/build-docker.sh
+++ docker/build-docker.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-podman build -t quay.io/abranzyck/sshd-bastion:ubuntu3 .
-podman push quay.io/abranzyck/sshd-bastion:ubuntu3
+podman build -t quay.io/abranzyck/sshd-bastion:ubuntu4 .
+podman push quay.io/abranzyck/sshd-bastion:ubuntu4
