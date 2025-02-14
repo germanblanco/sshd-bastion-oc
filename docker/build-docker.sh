fb967e92dec9117a1a813e57a69b6a549027692c
German Blanco <german.blanco@gmail.com>
2025-02-14 07:09:11 +0100

    changetag

--- docker/build-docker.sh
+++ docker/build-docker.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-podman build -t quay.io/abranzyck/sshd-bastion:ubuntu4 .
-podman push quay.io/abranzyck/sshd-bastion:ubuntu4
+podman build -t quay.io/abranzyck/sshd-bastion:ubuntu5 .
+podman push quay.io/abranzyck/sshd-bastion:ubuntu5
