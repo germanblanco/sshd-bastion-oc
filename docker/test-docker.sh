a2b353d8b320f78f1b31d4330879f2e1e021e286
German Blanco <german.blanco@ibm.com>
2025-02-14 09:36:01 +0100

    worksfortcp

--- docker/test-docker.sh
+++ docker/test-docker.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-podman run --rm -d --name sshd-bastion \
+podman run --rm -it --name sshd-bastion \
   -e SSH_KEY="${SSH_KEY}" \
   -p 2222:2222 -p 6343:6343/udp \
-  quay.io/abranzyck/sshd-bastion:ubuntu1
+  --user octest \
+  quay.io/abranzyck/sshd-bastion:arm_ubuntu5 /bin/bash
