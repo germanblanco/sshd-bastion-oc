90ea7ffefedb53a1d3836d6646a836200387a78d
German Blanco <german.blanco@gmail.com>
2025-02-17 15:57:01 +0100

    pamfails

--- docker_sshd/test-docker.sh
+++ docker_sshd/test-docker.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-podman run --rm -d --name sshd-bastion \
+podman run --rm -it --name sshd-bastion \
+  --user tunneluser \
   -e SSH_KEY="${SSH_KEY}" \
   -p 2222:2222 -p 6343:6343/udp \
-  quay.io/abranzyck/sshd-bastion:rhel_ubi1
+  quay.io/abranzyck/sshd-bastion:rhel_ubi1 /bin/bash
